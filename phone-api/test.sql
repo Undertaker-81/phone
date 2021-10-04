cd2433d2d73fa7ea3cd5fde51a0d8977f028a97f
panfilov.d <Undertaker-81@mail.ru>
2021-10-04 11:00:58 +0500

    fix table

--- phone-api/test.sql
+++ phone-api/test.sql
@@ -7,7 +7,9 @@ create table phone_numbers
     address      varchar(1000) null,
     phone_number varchar(50)   null
 );
+create unique index phone_numbers_phone_number_uindex
+    on phone_numbers (phone_number);
 
 insert into phone_numbers (fio, snils, phone_number, address)
 VALUES ('Дима', '45866655', '+79058547199', 'Kurgan'),
-       ('Коля', '45866655', '+79058547555', 'Moscow')
+       ('Коля', '45866655', '+79058547555', 'Moscow');
